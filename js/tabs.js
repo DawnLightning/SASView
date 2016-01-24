109cf4d22d0a84fec28cccfeb6282cf6b9b7cbbd
myloverhxx <myloverhxx@163.com>
2016-01-25 01:37:03 +0800

    set js

--- js/tabs.js
+++ js/tabs.js
@@ -16,3 +16,22 @@ $(document).ready(function() {
     });  
   
 });  
+
+$(document).ready(function() {  
+  
+    //Default Action  
+    $(".set-tab_content").hide(); //Hide all content  
+    $("ul.set-tabs li:first").addClass("active").show(); //Activate first tab  
+    $(".set-tab_content:first").show(); //Show first tab  content  
+      
+    //On Click Event  
+    $("ul.set-tabs li").click(function() {  
+        $("ul.set-tabs li").removeClass("active"); //Remove any "active" class  
+        $(this).addClass("active"); //Add "active" class to selected tab  
+        $(".set-tab_content").hide(); //Hide all tab content  
+        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content  
+        $(activeTab).fadeIn(); //Fade in the active content  
+        return false;  
+    });  
+  
+});  

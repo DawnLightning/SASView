3bccecfc079fd117a2e1e87e970bbf85b3c429b6
myloverhxx <myloverhxx@163.com>
2016-01-25 14:55:54 +0800

    schedule js

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+/*confirm*/
 $(document).ready(function() {  
     //Default Action  
     $(".tab_content").hide(); //Hide all content  
@@ -16,7 +17,7 @@ $(document).ready(function() {
     });  
   
 });  
-
+/*set*/
 $(document).ready(function() {  
   
     //Default Action  
@@ -29,6 +30,26 @@ $(document).ready(function() {
         $("ul.set-tabs li").removeClass("active"); //Remove any "active" class  
         $(this).addClass("active"); //Add "active" class to selected tab  
         $(".set-tab_content").hide(); //Hide all tab content  
+        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content  
+        $(activeTab).fadeIn(); //Fade in the active content  
+        return false;  
+    });  
+  
+});  
+/*schedule*/
+<script type="text/javascript">   
+$(document).ready(function() {  
+  
+    //Default Action  
+    $(".sch-tab_content").hide(); //Hide all content  
+    $("ul.sch-tabs li:first").addClass("active").show(); //Activate first tab  
+    $(".sch-tab_content:first").show(); //Show first tab  content  
+      
+    //On Click Event  
+    $("ul.sch-tabs li").click(function() {  
+        $("ul.sch-tabs li").removeClass("active"); //Remove any "active" class  
+        $(this).addClass("active"); //Add "active" class to selected tab  
+        $(".sch-tab_content").hide(); //Hide all tab content  
         var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content  
         $(activeTab).fadeIn(); //Fade in the active content  
         return false;  

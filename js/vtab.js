c8ba5cae09da7cf8354f86e5c209a383aeeae730
leeohj <leeohj@163.com>
2016-01-29 15:41:23 +0800

    button

--- js/vtab.js
+++ js/vtab.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+ // JavaScript Document
 function g(o){return document.getElementById(o);}
 function hoverli(n){
 for(var i=1;i<=11;i++){g('tb_'+i).className='aa';g('tbc_0'+i).className='undis';}g('tbc_0'+n).className='list2';g('tb_'+n).className='bb';

f1d22f6f5892ade085a1f012c1b0913a8c3675a5
leeohj <leeohj@163.com>
2016-01-30 13:53:06 +0800

    部分全选

---
+++ js/selectcheckbox.js
@@ -0,0 +1,234 @@
+// JavaScript Document
+//monday
+function selectmonday() 
+{ 
+var allCheckBoxs = document.getElementsByName("mon"); 
+var desc = document.getElementById("monday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+
+//tuesday
+function selecttuesday() 
+{ 
+var allCheckBoxs = document.getElementsByName("tue"); 
+var desc = document.getElementById("tuesday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//wednesday
+function selectwednesday() 
+{ 
+var allCheckBoxs = document.getElementsByName("wed"); 
+var desc = document.getElementById("wednesday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//thursday
+function selectthursday() 
+{ 
+var allCheckBoxs = document.getElementsByName("thu"); 
+var desc = document.getElementById("thursday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//friday
+function selectfriday() 
+{ 
+var allCheckBoxs = document.getElementsByName("fri"); 
+var desc = document.getElementById("friday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//saturday
+function selectsaturday() 
+{ 
+var allCheckBoxs = document.getElementsByName("sat"); 
+var desc = document.getElementById("saturday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//sunday
+function selectsunday() 
+{ 
+var allCheckBoxs = document.getElementsByName("sun"); 
+var desc = document.getElementById("sunday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//all

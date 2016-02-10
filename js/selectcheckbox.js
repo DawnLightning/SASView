ba28ef51048b738de4324f2610702fe45a012c49
myloverhxx <myloverhxx@163.com>
2016-02-10 21:25:44 +0800

    未完成

--- js/selectcheckbox.js
+++ js/selectcheckbox.js
@@ -1,33 +1,234 @@
 // JavaScript Document
-
-function selectallweek(thisbox,thisform) {
-  var check = thisbox.checked;
-  // 遍历 form  
-  for (var i = 0; i < thisform.elements.length; i++) {
-    // 提取控件  
-    var checkbox = thisform.elements[i];
-    // 检查是否是指定的控件  
-    if (checkbox.name != thisbox.name && checkbox.type === "checkbox" && checkbox.checked != check) {
-      checkbox.checked = check;
-    }
-  }
+//monday
+function selectmonday() 
+{ 
+var allCheckBoxs = document.getElementsByName("mon"); 
+var desc = document.getElementById("monday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
 }
 
-function selectday(thisday, dayname) {
-  var allCheckBoxs = document.getElementsByName(dayname);
-  var selectOrUnselect = true;
-  if (thisday.checked) {
-    selectOrUnselect = false;
-  }
-
-  if (selectOrUnselect) {
-
-    for (var i = 0; i < allCheckBoxs.length; i++) {
-      allCheckBoxs[i].checked = false;
-    }
-  } else {
-    for (var i = 0; i < allCheckBoxs.length; i++) {
-      allCheckBoxs[i].checked = true;
-    }
-  }
+//tuesday
+function selecttuesday() 
+{ 
+var allCheckBoxs = document.getElementsByName("tue"); 
+var desc = document.getElementById("tuesday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
 }
+//wednesday
+function selectwednesday() 
+{ 
+var allCheckBoxs = document.getElementsByName("wed"); 
+var desc = document.getElementById("wednesday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//thursday
+function selectthursday() 
+{ 
+var allCheckBoxs = document.getElementsByName("thu"); 
+var desc = document.getElementById("thursday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//friday
+function selectfriday() 
+{ 
+var allCheckBoxs = document.getElementsByName("fri"); 
+var desc = document.getElementById("friday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//saturday
+function selectsaturday() 
+{ 
+var allCheckBoxs = document.getElementsByName("sat"); 
+var desc = document.getElementById("saturday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//sunday
+function selectsunday() 
+{ 
+var allCheckBoxs = document.getElementsByName("sun"); 
+var desc = document.getElementById("sunday"); 
+var selectOrUnselect=false; 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+if(allCheckBoxs[i].checked){ 
+selectOrUnselect=true; 
+break; 
+} 
+} 
+if (selectOrUnselect) 
+{ 
+_allUnchecked(allCheckBoxs); 
+}else 
+{ 
+_allchecked(allCheckBoxs); 
+} 
+} 
+function _allchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = true; 
+} 
+} 
+function _allUnchecked(allCheckBoxs){ 
+for(var i = 0; i < allCheckBoxs.length; i ++ ) 
+{ 
+allCheckBoxs[i].checked = false; 
+} 
+}
+//all
--- js/tabs.js
+++ js/tabs.js
@@ -1,88 +1,106 @@
 // JavaScript Document
-$(document).ready(function() {
-  $(".mm-menu__link").on("click",function(){
-    //Default Action
-    $(".tab_content").hide(); //Hide all content
-    $("ul.tabs li:first").addClass("active").show(); //Activate first tab
-    $(".tab_content:first").show(); //Show first tab  content
-    //Default Action
-    $(".set-tab_content").hide(); //Hide all content
-    $("ul.set-tabs li:first").addClass("active").show(); //Activate first tab
-    $(".set-tab_content:first").show(); //Show first tab  content
-  });
+$(document).ready(function() {  
+    //Default Action  
+    $(".tab_content").hide(); //Hide all content  
+    $("ul.tabs li:first").addClass("active").show(); //Activate first tab  
+    $(".tab_content:first").show(); //Show first tab  content  
+      
+    //On Click Event  
+    $("ul.tabs li").click(function() {  
+        $("ul.tabs li").removeClass("active"); //Remove any "active" class  
+        $(this).addClass("active"); //Add "active" class to selected tab  
+        $(".tab_content").hide(); //Hide all tab content  
+        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content  
+        $(activeTab).fadeIn(); //Fade in the active content  
+        return false;  
+    });  
+  
+});  
 
-    //On Click Event
-  $(document).on("click","ul.tabs li",function(){
-        $("ul.tabs li").removeClass("active"); //Remove any "active" class
-        $(this).addClass("active"); //Add "active" class to selected tab
-        $(".tab_content").hide(); //Hide all tab content
-        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
-        $(activeTab).fadeIn(); //Fade in the active content
-        return false;
-  });
-
-
-
-    //On Click Event
-  $(document).on("click","ul.set-tabs li",
-    function() {
-        $("ul.set-tabs li").removeClass("active"); //Remove any "active" class
-        $(this).addClass("active"); //Add "active" class to selected tab
-        $(".set-tab_content").hide(); //Hide all tab content
-        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
-        $(activeTab).fadeIn(); //Fade in the active content
-        return false;
-    });
-
-
+$(document).ready(function() {  
+  
+    //Default Action  
+    $(".set-tab_content").hide(); //Hide all content  
+    $("ul.set-tabs li:first").addClass("active").show(); //Activate first tab  
+    $(".set-tab_content:first").show(); //Show first tab  content  
+      
+    //On Click Event  
+    $("ul.set-tabs li").click(function() {  
+        $("ul.set-tabs li").removeClass("active"); //Remove any "active" class  
+        $(this).addClass("active"); //Add "active" class to selected tab  
+        $(".set-tab_content").hide(); //Hide all tab content  
+        var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content  
+        $(activeTab).fadeIn(); //Fade in the active content  
+        return false;  
+    });  
+  
+});  
 
 /* =============================================================================
- table add & delete
- ============================================================================= */
+  table add & delete
+============================================================================= */
+window.onload=function()
+{
+var oTab=document.getElementById('tb1');
+var oBtn=document.getElementById('btn1');
+var oNo=document.getElementById('teacherNo');
+var oName=document.getElementById('teacherName');
+var oTitle=document.getElementById('teacherTitle');
+var oRoom=document.getElementById('teacherRoom');
+var oTel=document.getElementById('teacherTel');
+var oEmail=document.getElementById('teacherEmail');
+oBtn.onclick=function()
+{
+var oTr=document.createElement('tr');//创建一个tr   
+//创建第一个td   
+var oTd=document.createElement('td');
+oTd.innerHTML=oNo.value;
+oTr.appendChild(oTd);//将td插入tr
+//创建第二个td
+var oTd=document.createElement('td');
+oTd.innerHTML=oName.value;
+oTr.appendChild(oTd);//将td插入tr
+//创建第三个td
+var oTd=document.createElement('td');
+oTd.innerHTML=oTitle.value;
+oTr.appendChild(oTd);//将td插入tr
+//创建第四个td
+var oTd=document.createElement('td');
+oTd.innerHTML=oRoom.value;
+oTr.appendChild(oTd);//将td插入tr
+//创建第五个td
+var oTd=document.createElement('td');
+oTd.innerHTML=oTel.value;
+oTr.appendChild(oTd);//将td插入tr
+//创建第六个td
+var oTd=document.createElement('td');
+oTd.innerHTML=oEmail.value;
+oTr.appendChild(oTd);//将td插入tr
 
-  $(document).on("click","btn1",function(){
-    var oTab = document.getElementById('tb1');
+oTab.tBodies[0].appendChild(oTr);//将整个tr插入到表格中
+}
 
-    var oNo = document.getElementById('teacherNo');
-    var oName = document.getElementById('teacherName');
-    var oTitle = document.getElementById('teacherTitle');
-    var oRoom = document.getElementById('teacherRoom');
-    var oTel = document.getElementById('teacherTel');
-    var oEmail = document.getElementById('teacherEmail');
+}
+//然并卵up
 
-    var oTr = document.createElement('tr'); //创建一个tr
-        //创建第一个td
-        var oTd = document.createElement('td');
-        oTd.innerHTML = oNo.value;
-        oTr.appendChild(oTd); //将td插入tr
-        //创建第二个td
-        var oTd = document.createElement('td');
-        oTd.innerHTML = oName.value;
-        oTr.appendChild(oTd); //将td插入tr
-        //创建第三个td
-        var oTd = document.createElement('td');
-        oTd.innerHTML = oTitle.value;
-        oTr.appendChild(oTd); //将td插入tr
-        //创建第四个td
-        var oTd = document.createElement('td');
-        oTd.innerHTML = oRoom.value;
-        oTr.appendChild(oTd); //将td插入tr
-        //创建第五个td
-        var oTd = document.createElement('td');
-        oTd.innerHTML = oTel.value;
-        oTr.appendChild(oTd); //将td插入tr
-        //创建第二个td
-        var oTd = document.createElement('td');
-        oTd.innerHTML = oEmail.value;
-        oTr.appendChild(oTd); //将td插入tr
-        oTab.tBodies[0].appendChild(oTr); //将整个tr插入到表格中
-  });
+function show()
+{
+var value = document.getElementById("div1").style.display;
+if(value=="none")
+{
+document.getElementById("div1").style.display="block";
+}
+else
+document.getElementById("div1").style.display="none";
+}
 
-});
-
-function show() {
-    var value = document.getElementById("div1").style.display;
-    if (value == "none") {
-        document.getElementById("div1").style.display = "block";
-    } else document.getElementById("div1").style.display = "none";
+function show2()
+{
+var value = document.getElementById("div2").style.display;
+if(value=="none")
+{
+document.getElementById("div2").style.display="inline-block";
+}
+else
+document.getElementById("div2").style.display="none";
 }
